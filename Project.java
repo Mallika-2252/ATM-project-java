5274cdc83761b618be4b5f8874b1d4e60b35d055
Mallika <83360621+Mallika-2252@users.noreply.github.com>
2022-09-01 15:30:01 +0530

    Project.java

---
+++ Project.java
@@ -0,0 +1,21 @@
+package test;
+import java.util.*;
+import java.io.*;
+public class Project extends BankJava
+{
+Project(String cname, int cid)
+{
+		super(cname, cid);
+		// TODO Auto-generated constructor stub
+
+}
+
+public static void main(String[] args)
+{
+	BankJava obj1 = new BankJava("MALLIKA",316);
+    obj1.showmenu();
+}
+}
+
+
+
